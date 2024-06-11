574e6947bd8badbece4620dc970345cf5cb60ba4
FLinLan <maxlan0987@gmail.com>
2024-06-10 18:40:25 -0700

    added login page component and login button

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -3,14 +3,16 @@ import Logo from './Logo'
 import { GrSearch } from "react-icons/gr";
 import { FaRegCircleUser } from "react-icons/fa6";
 import { FaShoppingCart } from "react-icons/fa";
-
+import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <header className='h-16 shadow-md'>
+    <header className='h-16 shadow-md bg-white'>
       <div className='h-full container mx-auto flex items-center px-4 justify-between'>
         <div className=''>
-          <Logo w={100} h={60}/>
+          <Link to={"/"}>
+            <Logo w={100} h={60}/>
+          </Link>
         </div>
 
         <div className='hidden lg:flex items-center w-full justify-between max-w-sm border rounded-full focus-within:shadow-p1-2'>
@@ -34,7 +36,7 @@ const Header = () => {
           </div>
 
           <div>
-            <button className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</button>
+            <Link to={"/login"} className='px-3 py-1 rounded-full text-white bg-red-600 hover:bg-red-700'>Login</Link>
           </div>
         </div>
 
