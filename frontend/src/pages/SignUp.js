2fae29d9b6e0e75eb7489cc9468fc9c32dba87ac
FLinLan <maxlan0987@gmail.com>
2024-06-12 23:13:04 -0700

    added backend dependencies

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -30,7 +30,12 @@ const SignUp = () => {
     const file = e.target.files[0]
 
     const imagePic = await imageTobase64(file)
-    console.log("imagePic", imagePic)
+    setData((prev)=>{
+      return{
+        ...prev,
+        profilePic : imagePic
+      }
+    })
   }
 
   const handleSubmit = (e) => {
@@ -45,7 +50,7 @@ const SignUp = () => {
           <div className='bg-white p-5 w-full max-w-sm mx-auto'>
               <div className= 'w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
                 <div>
-                  <img src={loginIcons} alt='login icons' />
+                  <img src={data.profilePic || loginIcons} alt='login icons' />
                 </div>
 
                 <form>
@@ -59,7 +64,7 @@ const SignUp = () => {
 
               </div>
 
-              <form className='pt-6 flex flex-col gap-2'>
+              <form className='pt-6 flex flex-col gap-2' onSubmit={handleSubmit}>
               <div className='grid'>
                       <label>Name :</label>
                       <div  className='bg-slate-100 p-2'>
@@ -68,6 +73,7 @@ const SignUp = () => {
                               placeholder='enter your name' 
                               name='text'
                               onChange={handleOnChange}
+                              required
                               className='w-full h-full outline-none bg-transparent' />
                       </div>
                   </div>
@@ -80,6 +86,7 @@ const SignUp = () => {
                               placeholder='enter email' 
                               name='email'
                               onChange={handleOnChange}
+                              required
                               className='w-full h-full outline-none bg-transparent' />
                       </div>
                   </div>
@@ -93,6 +100,7 @@ const SignUp = () => {
                               value={data.password}
                               name='password'
                               onChange={handleOnChange}
+                              required
                               className='w-full h-full outline-none bg-transparent'/>
                           <div className='cursor-pointer text-xl' onClick={()=>setShowPassword((prev)=>!prev)}>
                               <span>
